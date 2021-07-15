28779b777c8203c4ea74f402b1db84ec7d52f351
BaccanoB <beatrice.baccano@yahoo.it>
2021-07-15 17:54:31 +0200

    impaginata home page con anche scss

--- resources/views/holiday.blade.php
+++ resources/views/holiday.blade.php
@@ -3,25 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset ("css/app.css")}}">
 
         <title>Laravel</title>
 
     </head>
     <body>
+        <h1>Scegli il pacchetto vacanze che preferisci!</h1>
         <section class="container">
-            @foreach ($holidays as $item)
+                @foreach ($holidays as $item)
+                
                 <div class="card">
+                    <div class="transparency">
                     <h2>{{ $item->name }}</h2>
                     <h3>Viaggia con {{$item->holiday_agency}}</h3>
                     <p>Paese: {{$item->country}}</p>
                     <h4>Arriverai a {{$item->city}} e soggiornerai presso l'hotel {{$item->hotel}}</h4>
-                    <p>{{$item->description}}</p>
-                    <small>La data di partenza è prevista per il {{$item->departure}}</small>
-                    <small>La data di ritorno è prevista per il {{$item->return}}</small>
-                    <p>Il prezzo del soggiorno è di {{$item->price}} &euro;</p>
+                    <p class="description"> <em>{{$item->description}}</em> </p>
+                    <small>La data di partenza è prevista per il <strong>{{$item->departure}}</strong></small>
+                    <small>La data di ritorno è prevista per il <strong>{{$item->return}}</strong></small>
+                    <p>Il prezzo del soggiorno è di <strong>{{$item->price}} &euro;</strong></p>
                 </div>
-                
+                </div>   
             @endforeach
+            
         </section>
     </body>
 </html>
